3ad503129869b99a12d3ddf7e2c2067f410ece06
MikaelMagal <mikaelmagalhaes28@gmail.com>
2026-05-27 11:14:10 -0300

    add bonus 67

--- commands/farmar.js
+++ commands/farmar.js
@@ -13,13 +13,21 @@ async function farmar(interaction) {
     }
 
     const userid = interaction.user.id;
-    const ganho  = result.ganho;
+    let bonus = 0;
+    let mensagem_bonus = "";
+
+    if (result.ganho === 67) {
+        mensagem_bonus = "**BÔNUS DE SORTE!** 🎉\n Você ganhou + 1000 aura pelo 67!\n";
+        bonus = 1000;
+    }
+    const ganho  = result.ganho + bonus;
 
     userService.addAura(userid, ganho);
 
     return interaction.reply({
         content:
-            `🌟 Você coletou **+${result.ganho} aura** hoje!\n` +
+            mensagem_bonus + 
+            `🌟 Você coletou **+${ganho} aura** hoje!\n` +
             `Volte amanhã para mais.`
     });
 }
